0335ad1e1851df530e3d9cd14eb854035c5fc0af
yogesh130 <yogesh@resynctech.com>
2022-10-08 23:34:24 +0800

    edited game

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState, useEffect, Component } from "react";
 import { motion } from "framer-motion";
 
 import { variants } from "../assets/Animations";
@@ -6,51 +6,102 @@ import GameService from "../services/GameService";
 import thegreeninvestor from "../assets/thegreeninvestor.png";
 import DataMetric from "../components/DataMetric/DataMetric";
 import { CashIcon, MoraleIcon, SustainabilityIcon } from "../icons";
-import { Box } from "@mantine/core";
+import { Box, Grid, Text, Button, Group } from "@mantine/core";
 
-export default class Game extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      //   content: ""
-      // data: []
-    };
+import authHeader from "../services/auth-header";
+import image1 from "../assets/img1.jpg";
+
+import axios from "axios";
+
+export default function Game() {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+  const [question, setQuestion] = useState();
+  const [options, setOptions] = useState();
+
+  useEffect(() => {
+    setLoading(true);
+    async function getAllData() {
+      await axios
+        .get("http://localhost:8080/api/questions", {
+          headers: authHeader(),
+          "Content-Type": "application/json",
+        })
+        .then(async (response) => {
+          await setData(response.data);
+          await setQuestion(response.data[0]);
+          return axios.get(`http://localhost:8080/api/questions/1/options`);
+        })
+        .then((response) => {
+          setOptions(response.data);
+        })
+        .then(() => {
+          setLoading(false);
+        })
+        .catch((error) => console.log(error.response));
+    }
+    getAllData();
+  }, []);
+  if (data === undefined || question === undefined || options === undefined) {
+    return <>Still loading...</>;
   }
 
-  componentDidMount() {
-    // GameService.getGameContent().then(
-    //   response => {
-    //     this.setState({
-    //       data: response.data
-    //     });
-    //   },
-    //   error => {
-    //     this.setState({
-    //       content:
-    //         (error.response && error.response.data) ||
-    //         error.message ||
-    //         error.toString()
-    //     });
-    //   }
-    // );
-  }
+  return (
+    <motion.div
+      initial="hidden"
+      animate="visible"
+      exit="hidden"
+      variants={variants}
+    >
+      <Box className="bg-gray-50 bg-opacity-70 h-[85vh] rounded-xl align-middle w-full pt-2 pr-2 pl-2 pb-2">
+        <Grid className="h-full w-full">
+          <Grid.Col span={7} className="h-full">
+            <Box className="h-[55%] w-full flex flex-col items-center space-y-2">
+              <Text className="text-center font-semibold text-xl">
+                {question.question}
+              </Text>
+              <img
+                className="h-[70%] w-[70%] text-center"
+                src={require("../assets/img1.jpg")}
+                alt="new"
+              />
+            </Box>
+            <Box
+              size="md"
+              className="h-[45%] w-full flex flex-col items-center space-y-2"
+            >
+              <Button
+                size="md"
+                className="w-[80%] h-full opacity-80 bg-gray-50 text-black"
+              >
+                {options[0]}
+              </Button>
+              <Button
+                size="md"
+                className="w-[80%] h-full opacity-80 bg-gray-50 text-black"
+              >
+                Placeholder
+              </Button>
+              <Button
+                size="md"
+                className="w-[80%] h-full opacity-80 bg-gray-50 text-black"
+              >
+                Placeholder
+              </Button>
+              <Button
+                size="md"
+                className="w-[80%] h-full opacity-80 bg-gray-50 text-black"
+              >
+                Placeholder
+              </Button>
 
-  render() {
-    return (
-      <motion.div
-        initial="hidden"
-        animate="visible"
-        exit="hidden"
-        variants={variants}
-      >
-        <img src={thegreeninvestor} alt="thegreeninvestorlogo" />
-        <div className="RectangleQuestions center">
-          <div className="q-container">
-            <div className="quodrant1">
-              <h1 className="text-center">Placeholder</h1>
-              <p className="text-center">Placeholder</p>
-            </div>
-            <div className="quodrant2">
+              <Button size="md" className="h-[90%] bg-darkGreen-50 text-black">
+                Submit
+              </Button>
+            </Box>
+          </Grid.Col>
+          <Grid.Col span={5} className="h-full w-full space-y-2">
+            <Box className="h-[53%] w-full space-y-2">
               <DataMetric
                 hasChart={true}
                 icon={<CashIcon color="grey" className="text-xl" />}
@@ -59,40 +110,76 @@ export default class Game extends Component {
                 unit={"SGD"}
                 label="Cash"
               />
-              {/* <h2 className="text-center">Placeholder for more text here</h2> */}
-            </div>
-            <div className="quodrant3">
-              <button className="block glow-button">btn1</button>
-              <button className="block glow-button">btn2</button>
-              <button className="block glow-button">btn3</button>
-              <button className="block glow-button">btn4</button>
-            </div>
-            <div className="quodrant4">
-              <Box className="h-[33vh] space-x-4 flex flex-row">
-                <DataMetric
-                  increment={-5}
-                  icon={<MoraleIcon color="grey" className="text-xl" />}
-                  value={40}
-                  unit={"%"}
-                  label="Morale"
-                />
-                <DataMetric
-                  icon={<SustainabilityIcon color="grey" className="text-xl" />}
-                  increment={7}
-                  value={54}
-                  unit={"%"}
-                  label="Sustainability"
-                />
-              </Box>
-              {/* <div className='barcontainer'>
-                                <div className='bar' style={{ height: '75%', background: '#EA6042' }}>75%</div>
-                                <div className='bar' style={{ height: '25%', background: '#86E577' }}>25%</div>
-                                <div className='bar' style={{ height: '50%', background: '#ECF029' }}>50%</div>
-                            </div> */}
-            </div>
-          </div>
-        </div>
-      </motion.div>
-    );
-  }
+            </Box>
+
+            <Box className="h-[47%] w-full flex flex-row space-x-2">
+              <DataMetric
+                increment={-5}
+                icon={<MoraleIcon color="grey" className="text-xl" />}
+                value={40}
+                unit={"%"}
+                label="Morale"
+              />
+              <DataMetric
+                icon={<SustainabilityIcon color="grey" className="text-xl" />}
+                increment={7}
+                value={54}
+                unit={"%"}
+                label="Sustainability"
+              />
+            </Box>
+          </Grid.Col>
+        </Grid>
+      </Box>
+    </motion.div>
+  );
 }
+
+// {/* <div className="RectangleQuestions center">
+// <div className="q-container">
+//   <div className="quodrant1">
+//     <h1 className="text-center">Placeholder</h1>
+//     <p className="text-center">Placeholder</p>
+//   </div>
+//   <div className="quodrant2">
+//     <DataMetric
+//       hasChart={true}
+//       icon={<CashIcon color="grey" className="text-xl" />}
+//       increment={5}
+//       value={40}
+//       unit={"SGD"}
+//       label="Cash"
+//     />
+//     {/* <h2 className="text-center">Placeholder for more text here</h2> */}
+//   </div>
+//   <div className="quodrant3">
+//     <button className="block glow-button">btn1</button>
+//     <button className="block glow-button">btn2</button>
+//     <button className="block glow-button">btn3</button>
+//     <button className="block glow-button">btn4</button>
+//   </div>
+//   <div className="quodrant4">
+//     <Box className="h-[33vh] space-x-4 flex flex-row">
+//       <DataMetric
+//         increment={-5}
+//         icon={<MoraleIcon color="grey" className="text-xl" />}
+//         value={40}
+//         unit={"%"}
+//         label="Morale"
+//       />
+//       <DataMetric
+//         icon={<SustainabilityIcon color="grey" className="text-xl" />}
+//         increment={7}
+//         value={54}
+//         unit={"%"}
+//         label="Sustainability"
+//       />
+//     </Box>
+//     {/* <div className='barcontainer'>
+//                       <div className='bar' style={{ height: '75%', background: '#EA6042' }}>75%</div>
+//                       <div className='bar' style={{ height: '25%', background: '#86E577' }}>25%</div>
+//                       <div className='bar' style={{ height: '50%', background: '#ECF029' }}>50%</div>
+//                   </div> */}
+//   </div>
+// </div>
+// </div> */}
--- src/services/GameService.js
+++ src/services/GameService.js
@@ -1,25 +1,36 @@
-import axios from 'axios';
-import authHeader from './auth-header';
+import axios from "axios";
+import authHeader from "./auth-header";
 
-const API_URL = 'http://localhost:8080/api/';
+const API_URL = "http://localhost:8080/api/";
 
 class GameService {
-    getGameContent() {
-        // return axios.get(API_URL + 'gameStats', { headers: authHeader(), 'Content-Type': 'application/json' });
-        return "dummytext";
-    }
+   getGameContent() {
+    return axios.get(API_URL + "questions", {
+      headers: authHeader(),
+      "Content-Type": "application/json",
+    });
+    //return "dummytext";
+  }
 
-    //   getUserBoard() {
-    //     return axios.get(API_URL + 'user', { headers: authHeader() });
-    //   }
+  getOptions({id}) {
+    return axios.get(API_URL + `questions/${id}/options`, {
+      headers: authHeader(),
+      "Content-Type": "application/json",
+    });
+    //return "dummytext";
+  }
 
-    //   getModeratorBoard() {
-    //     return axios.get(API_URL + 'mod', { headers: authHeader() });
-    //   }
+  //   getUserBoard() {
+  //     return axios.get(API_URL + 'user', { headers: authHeader() });
+  //   }
 
-    //   getAdminBoard() {
-    //     return axios.get(API_URL + 'admin', { headers: authHeader() });
-    //   }
+  //   getModeratorBoard() {
+  //     return axios.get(API_URL + 'mod', { headers: authHeader() });
+  //   }
+
+  //   getAdminBoard() {
+  //     return axios.get(API_URL + 'admin', { headers: authHeader() });
+  //   }
 }
 
 export default new GameService();
