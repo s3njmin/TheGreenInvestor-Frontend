c8ca5e11dd56031eb0d4b7afde43705a73c6128c
FunAndSimple <edwin.chong.2021@scis.smu.edu.sg>
2022-10-09 23:37:55 +0800

    Changes to Nav Bar
    Replaced 'Green App' with 'Home'

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,128 +5,126 @@ import "../App.css";
 import "../css/barchart.css";
 import "../css/navbar.css";
 
-
 import thegreeninvestor from "../assets/thegreeninvestor.png";
-
 
 import AuthService from "../services/auth.service";
 // import AuthVerify from "./common/auth-verify";
 import EventBus from "../common/EventBus";
 
 export default class Game extends Component {
-    constructor(props) {
-        super(props);
-        this.logOut = this.logOut.bind(this);
+  constructor(props) {
+    super(props);
+    this.logOut = this.logOut.bind(this);
 
-        this.state = {
-            showModeratorBoard: false,
-            showAdminBoard: false,
-            currentUser: undefined,
-        };
+    this.state = {
+      showModeratorBoard: false,
+      showAdminBoard: false,
+      currentUser: undefined,
+    };
+  }
+
+  componentDidMount() {
+    const user = AuthService.getCurrentUser();
+
+    if (user) {
+      this.setState({
+        currentUser: user,
+        showModeratorBoard: user.roles.includes("ROLE_MODERATOR"),
+        showAdminBoard: user.roles.includes("ROLE_ADMIN"),
+      });
     }
 
-    componentDidMount() {
-        const user = AuthService.getCurrentUser();
+    EventBus.on("logout", () => {
+      this.logOut();
+    });
+  }
 
-        if (user) {
-            this.setState({
-                currentUser: user,
-                showModeratorBoard: user.roles.includes("ROLE_MODERATOR"),
-                showAdminBoard: user.roles.includes("ROLE_ADMIN"),
-            });
-        }
+  componentWillUnmount() {
+    EventBus.remove("logout");
+  }
 
+  logOut() {
+    AuthService.logout();
+    this.setState({
+      showModeratorBoard: false,
+      showAdminBoard: false,
+      currentUser: undefined,
+    });
+  }
 
-        EventBus.on("logout", () => {
-            this.logOut();
-        });
-    }
+  render() {
+    const { currentUser, showModeratorBoard, showAdminBoard } = this.state;
 
-    componentWillUnmount() {
-        EventBus.remove("logout");
-    }
+    return (
+      <nav className="navbar navbar-expand bg-gradient-to-r from-transparent via-teal-400/80 border-bottom-line">
+        <div className="navbar-nav mr-auto">
+          <li className="nav-item">
+            <Link to={"/home"} className="nav-link">
+              Home
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to={"/leaderboard"} className="nav-link">
+              Leaderboard
+            </Link>
+          </li>
 
-    logOut() {
-        AuthService.logout();
-        this.setState({
-            showModeratorBoard: false,
-            showAdminBoard: false,
-            currentUser: undefined,
-        });
-    }
+          {showModeratorBoard && (
+            <li className="nav-item">
+              <Link to={"/mod"} className="nav-link">
+                Moderator Board
+              </Link>
+            </li>
+          )}
 
+          {showAdminBoard && (
+            <li className="nav-item">
+              <Link to={"/admin"} className="nav-link">
+                Admin Board
+              </Link>
+            </li>
+          )}
 
-    render() {
-        const { currentUser, showModeratorBoard, showAdminBoard } = this.state;
+          {currentUser && (
+            <li className="nav-item">
+              <Link to={"/user"} className="nav-link">
+                User
+              </Link>
+            </li>
+          )}
+        </div>
 
-        return (
-            <nav className="navbar navbar-expand bg-transparent border-bottom-line">
-                <Link to={"/"} className="navbar-brand">
-                    Green-App
-                </Link>
-                <div className="navbar-nav mr-auto">
-                    <li className="nav-item">
-                        <Link to={"/home"} className="nav-link">
-                            Home
-                        </Link>
-                    </li>
+        <img className="" src={thegreeninvestor} alt="thegreeninvestorlogo" />
 
-                    {showModeratorBoard && (
-                        <li className="nav-item">
-                            <Link to={"/mod"} className="nav-link">
-                                Moderator Board
-                            </Link>
-                        </li>
-                    )}
+        {currentUser ? (
+          <div className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <Link to={"/profile"} className="nav-link">
+                {currentUser.username}
+              </Link>
+            </li>
+            <li className="nav-item">
+              <a href="/login" className="nav-link" onClick={this.logOut}>
+                LogOut
+              </a>
+            </li>
+          </div>
+        ) : (
+          <div className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <Link to={"/login"} className="nav-link">
+                Log In
+              </Link>
+            </li>
 
-                    {showAdminBoard && (
-                        <li className="nav-item">
-                            <Link to={"/admin"} className="nav-link">
-                                Admin Board
-                            </Link>
-                        </li>
-                    )}
-
-                    {currentUser && (
-                        <li className="nav-item">
-                            <Link to={"/user"} className="nav-link">
-                                User
-                            </Link>
-                        </li>
-                    )}
-                </div>
-
-                <img className="" src={thegreeninvestor} alt="thegreeninvestorlogo"/>
-
-                {currentUser ? (
-                    <div className="navbar-nav ml-auto">
-                        <li className="nav-item">
-                            <Link to={"/profile"} className="nav-link">
-                                {currentUser.username}
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            <a href="/login" className="nav-link" onClick={this.logOut}>
-                                LogOut
-                            </a>
-                        </li>
-                    </div>
-                ) : (
-                    <div className="navbar-nav ml-auto">
-                        <li className="nav-item">
-                            <Link to={"/login"} className="nav-link">
-                                Login
-                            </Link>
-                        </li>
-
-                        <li className="nav-item">
-                            <Link to={"/register"} className="nav-link">
-                                Sign Up
-                            </Link>
-                        </li>
-                    </div>
-                )}
-            </nav>
-        );
-    }
+            <li className="nav-item">
+              <Link to={"/register"} className="nav-link">
+                Sign Up
+              </Link>
+            </li>
+          </div>
+        )}
+      </nav>
+    );
+  }
 }
